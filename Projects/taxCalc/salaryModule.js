5e34219bc991a87e68f57c66b1e167ef3b2ebce7
slsmphys@gmail.com <slsmphys@gmail.com>
2018-05-12 21:23:36 +0100

    Initial commit of the JavaScripts Project

---
+++ Projects/taxCalc/salaryModule.js
@@ -0,0 +1,16 @@
+var salary = function() {
+
+    var _salary;
+    var _personalAllowance;
+    var _nationalInsurance;
+    var _pension;
+    var _studentLoan;
+
+    calculateIncomeTax = function(salary, pension, age) {
+
+    };
+
+    return {
+        taxCalc: calculateIncomeTax
+    };
+}();
